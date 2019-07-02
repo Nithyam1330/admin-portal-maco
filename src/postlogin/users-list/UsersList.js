509241fb5b3a897df13922bad954f62e0b1013bc
sai kumar SKK <sai.korthivada@xcubelabs.com>
2019-07-02 12:58:47 +0530

    Column level search

--- src/postlogin/users-list/UsersList.js
+++ src/postlogin/users-list/UsersList.js
@@ -1,7 +1,7 @@
 import React, { Component } from 'react'
 import { searchFilterArrayOfJson } from '../../shared/services/custom-search/CustomSearch';
 import Pagination from '../../shared/components/pagination/Pagination';
-
+import './UsersList.css';
 class UsersList extends Component {
     constructor(props) {
         super(props);
@@ -12,7 +12,6 @@ class UsersList extends Component {
             editableFields: false
         }
         this.onChangePage = this.onChangePage.bind(this);
-
     }
 
     onChangePage = (pageOfItems) => {
@@ -22,11 +21,11 @@ class UsersList extends Component {
         });
     }
 
-    search = (event) => {
+    search = (event, header) => {
         this.setState({
             search: event.target.value
         }, () => {
-            const headers = ['value'];
+            const headers = [header];
             const searchedArray = searchFilterArrayOfJson(this.props.users, this.state.search, headers);
             this.setState({
                 users: searchedArray
@@ -72,23 +71,68 @@ class UsersList extends Component {
                 <table className="table table-striped">
                     <thead>
                         <tr>
-                            <th scope="col">#</th>
-                            <th scope="col">First</th>
-                            <th scope="col">Last</th>
+                            <th scope="col">
+                                <span>Name</span>
+                                <br></br>
+                                <input type="text" placeholder="Search by Name" onKeyUp={(event) => this.search(event, 'name')}></input>
+                            </th>
+
+                            <th scope="col">
+                                <span>Email</span>
+                                <br></br>
+                                <input type="text" placeholder="Search by Email" onKeyUp={(event) => this.search(event, 'email')}></input>
+                            </th>
+
+                            <th scope="col">
+                                <span>Phone Number</span>
+                                <br></br>
+                                <input type="text" placeholder="Search by Phone " onKeyUp={(event) => this.search(event, 'phoneNumber')}></input>
+                            </th>
+
+                            <th scope="col">
+                                <span>User Id</span>
+                                <br></br>
+                            </th>
+                            <th scope="col">
+                                <span>Work Out With Me Price</span>
+                                <br></br>
+                            </th>
+
+                            <th scope="col">
+                                <span>Personal Training Price</span>
+                                <br></br>
+                            </th>
+
+                            <th scope="col">
+                                <span>Trainer Activation</span>
+                                <br></br>
+                            </th>
+                            <th scope="col">
+                                <span>Block Status</span>
+                                <br></br>
+                            </th>
                         </tr>
                     </thead>
                     <tbody>
                         {
                             this.state.users && this.state.users.map((res) => (
-                                <React.Fragment key={res.id}>
+                                <React.Fragment key={res.userid}>
                                     <tr>
-                                        <th>{res.id}</th>
-                                        <th>{res.value}</th>
+                                        <th>{res.name}</th>
+                                        <th>{res.email}</th>
+                                        <th>{res.phoneNumber}</th>
+                                        <th>{res.userid}</th>
+                                        <th>{res.workOutWithMePrice}</th>
+                                        <th>{res.personalTrainingPrice}</th>
+                                        <th>
+                                            {res.trainer_activation ? 'Active' : 'Inactive'}
+                                        </th>
+
                                         <th>
                                             <div className="form-group">
-                                                <select className="form-control" value={res.status} onChange={(event) => this.statusChange(event, res)} disabled={!this.state.editableFields}>
-                                                    <option value='PENDING'>PENGING</option>
-                                                    <option value="COMPLETED">COMPLETED</option>
+                                                <select className="form-control" value={res.isBlocked} onChange={(event) => this.statusChange(event, res)} disabled={!this.state.editableFields}>
+                                                    <option value='BLOCKED'>BLOCKED</option>
+                                                    <option value="ACTIVE">ACTIVE</option>
                                                 </select>
                                             </div>
                                         </th>
